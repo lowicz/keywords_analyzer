96ecf28ede757bdf0983b9eaa918d3b9fdc3a992
lowicz <mc.lowicz@gmail.com>
2015-02-25 17:15:13 +0100

    testy

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -6,6 +6,11 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return "Hello, world!"
+	
+	
+@app.route('/welcome')
+def welcome():
+	return render_template("index.html")
 	
 if __name__ == '__main__':
 	app.run(debug=True)
